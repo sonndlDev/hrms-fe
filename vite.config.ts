5a80d8e74b69ae738da73fbb7b474634162a1955
PiTunDev <sonndl.dev@gmail.com>
2024-09-11 19:13:33 +0700

    update env

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,16 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+  build: {
+    watch: {
+      include: 'src/**', // Theo dõi các file thay đổi trong thư mục src
+    },
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]',
+      },
+    },
+  },
 });
